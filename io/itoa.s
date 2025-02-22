cb299ff5f7bf390426fc8ae8cb268e8fef5a68b6
yousabbbbb <yousabmenissy@gmail.com>
2025-02-22 16:52:27 +0200

    Improved itoa

--- io/itoa.s
+++ io/itoa.s
@@ -1,59 +1,56 @@
 /*
-    convert int into string
+    Convert an integer to a string.
 */
 
 .section .text
 .global itoa
 .type itoa, @function
-
 itoa: # itoa(int, buff)
-    movq  %rdi, %r9
+    movq  %rdi, %rax
 
-    testq  %rdi, %rdi
+    testq  %rax, %rax
     jns    1f
-
-    movq   %rdi, %rax
-    movq   $-1, %r9
-    imulq  %r9
-    movq   %rax, %r9
+    imul   $-1, %rax
 
     1:
     xor   %rcx, %rcx
     xor   %rdx, %rdx
     movq  $10, %r8
-    movq  %r9, %rax
 
     .LPITOA0:
-    cqo
-    div    %r8
-    addq   $'0', %rdx
-    pushq  %rdx
-    incq   %rcx
-
+    incq  %rcx
+    xor   %rdx, %rdx
+    div   %r8
+    addq  $'0', %rdx
+    movb  %dl, -1(%rsi, %rcx)
     cmpq  $0, %rax
     jne   .LPITOA0
 
     testq  %rdi, %rdi
     jns    1f
-    pushq  $'-'
     incq   %rcx
+    movb   $'-', -1(%rsi, %rcx)
 
     1:
-    xor   %rax, %rax
-    movq  %rcx, %r10
-    movq  $0, %rcx
+    movb  $0, (%rsi, %rcx) # Null terminate the string
+
+    # Reverse the string
+    movq  %rsi, %rax
+    addq  %rcx, %rax
+    decq  %rax
+    movq  %rax, %rdi
 
     .LPITOA1:
-    popq  %rax
-    movb  %al, (%rsi, %rcx)
-    addq  $1, %rcx
+    cmpq  %rsi, %rdi
+    jb    1f
+    movb  (%rsi), %al
+    movb  (%rdi), %dl
+    movb  %al, (%rdi)
+    movb  %dl, (%rsi)
+    incq  %rsi
+    decq  %rdi
+    jmp   .LPITOA1
 
-    cmpq  %rcx, %r10
-    jne   .LPITOA1
-
-    movq  $0, %rax          # The null character
-    movb  %al, (%rsi, %rcx)
-    addq  $1, %rcx
-
+    1:
     movq  %rcx, %rax
     ret
